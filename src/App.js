3d51757171d7981869aca41c815050fa67a49b83
omkar7750 <omkarkhenevar1@gmail.com>
2022-06-28 20:11:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Viewer, Worker } from '@react-pdf-viewer/core';
+
+// Plugins
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+// Import styles
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import { pdfjs } from 'react-pdf';
+
 function App() {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js`}>
+        <Viewer
+          fileUrl='/sample.pdf'
+          plugins={[
+              // Register plugins
+              defaultLayoutPluginInstance
+          ]}
+        />
+      </ Worker>
     </div>
   );
 }
