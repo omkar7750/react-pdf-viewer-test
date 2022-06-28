60027589b6117b0e3c8fde3a196fd51af50cf7d5
omkar7750 <omkarkhenevar1@gmail.com>
2022-06-28 21:00:45 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -9,13 +9,19 @@ import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 // Import styles
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import { pdfjs } from 'react-pdf';
+// import { pdfjs } from 'react-pdf';
+
+import('pdfjs-dist/build/pdf');
+import('pdfjs-dist/build/pdf.worker.entry');
+
+//  pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 function App() {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
   return (
     <div className="App">
-      <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js`}>
+      {/* <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js`}> */}
+      <Worker >
         <Viewer
           fileUrl='/sample.pdf'
           plugins={[
